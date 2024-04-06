a2d730d74cbbea66fd8d3c81cd9d9ce89353827c
Alexander Martirosov <alexxjk@gmail.com>
2024-04-06 12:04:42 +0400

    feat: Add jimp package for image manipulation
    This commit adds the `jimp` package as a new dependency to the project. The `jimp` package is used for image manipulation. It includes plugins for various image manipulation tasks such as blurring, cropping, scaling, and more. The `jimp` package will be used to enhance the functionality of the application.

--- squaregramio-app/server.js
+++ squaregramio-app/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import path from 'path';
 import {fileURLToPath} from 'url';
 import multer from 'multer';
+import Jimp from "jimp";
 
 import {BlobBatchClient, BlobServiceClient} from "@azure/storage-blob";
 
@@ -20,8 +21,11 @@ app.get('/', (req, res) => {
 
 app.post('/api/project/:projectName', upload.single('file'), (req, res) => {
 	uploadFile(req.params.projectName, req.body.connectionString, req.file)
+		.then(red => {
+			res.json();
+		})
 		.catch();
-	res.json();
+
 });
 
 app.get('/api/project/:projectName', (req, res) => {
@@ -57,5 +61,33 @@ async function uploadFile(containerName, connectionString, file) {
 	const options = {blobHTTPHeaders: {blobContentType: file.mimetype}};
 	const blobName = file.originalname;
 	let blockedBlobClient = containerClient.getBlockBlobClient(blobName);
-	return blockedBlobClient.upload(file.buffer, file.buffer.length, options);
+	let squareImage = await square(file);
+	console.log(squareImage);
+	console.log(squareImage.length);
+	return blockedBlobClient.upload(squareImage, squareImage.length, options);
+}
+
+async function square(file) {
+	try {
+		const image = await Jimp.read(file.buffer);
+		const size = Math.min(image.bitmap.width, image.bitmap.height);
+		const squareImage = new Jimp(size, size);
+		const x0 = Math.max(0, Math.floor((image.bitmap.width - size) / 2));
+		const y0 = Math.max(0, Math.floor((image.bitmap.height - size) / 2));
+		console.log(size);
+		console.log(x0);
+		console.log(y0);
+		image.scan(x0, y0, size, size, (x, y, idx) => {
+			const red = image.bitmap.data[idx];
+			const green = image.bitmap.data[idx + 1];
+			const blue = image.bitmap.data[idx + 2];
+			const alpha = image.bitmap.data[idx + 3];
+			squareImage.setPixelColor(Jimp.rgbaToInt(red, green, blue, alpha), x, y);
+		});
+		console.log(image.getMIME());
+		const buffer = await squareImage.getBufferAsync(image.getMIME());
+		return buffer;
+	} catch (err) {
+		console.log(err);
+	}
 }
